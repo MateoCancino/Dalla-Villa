6a8bfd88ed7b228ef8d4ac5523072335fab25eca
Mateo Cancino <mateoariel347@gmail.com>
2024-11-14 17:10:50 -0300

    Actualizando funcionalidad del Navbar

--- JS/script.js
+++ JS/script.js
@@ -1,6 +1,13 @@
 let menu = document.querySelector('#menu-icon');
-        let navlist = document.querySelector('.navbar');
-        menu.onclick = () =>{
-            menu.classList.toggle('bx-x');
-            navlist.classList.toggle('open');
+let navlist = document.querySelector('.navbar');
+let navLinks = document.querySelectorAll('.navbar__item a');
+    menu.onclick = () =>{
+        menu.classList.toggle('bx-x');
+        navlist.classList.toggle('open');
+    }
+    navLinks.forEach(link => {
+        link.onclick = () => {
+            menu.classList.remove('bx-x');
+            navlist.classList.remove('open');
         }
+    });
